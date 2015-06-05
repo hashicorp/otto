426efbe363cae5a4bae66f148b05882b78c2d9b8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-06-05 18:55:33 +0200

    otto: StyledUi properly indents all lines

--- otto/ui_styled.go
+++ otto/ui_styled.go
@@ -1,5 +1,12 @@
 package otto
 
+import (
+	"bufio"
+	"bytes"
+	"strings"
+	"unicode"
+)
+
 // StyledUi is a wrapper around an existing UI that automatically
 // adds formatting around the UI text.
 type StyledUi struct {
@@ -7,9 +14,21 @@ type StyledUi struct {
 }
 
 func (u *StyledUi) Header(msg string) {
-	u.Ui.Header("==> " + msg)
+	u.Ui.Header(u.prefix("==> ", msg))
 }
 
 func (u *StyledUi) Message(msg string) {
-	u.Ui.Message("    " + msg)
+	u.Ui.Message(u.prefix("    ", msg))
+}
+
+func (u *StyledUi) prefix(prefix, msg string) string {
+	var buf bytes.Buffer
+
+	scan := bufio.NewScanner(strings.NewReader(msg))
+	for scan.Scan() {
+		buf.WriteString(prefix)
+		buf.WriteString(scan.Text() + "\n")
+	}
+
+	return strings.TrimRightFunc(buf.String(), unicode.IsSpace)
 }
