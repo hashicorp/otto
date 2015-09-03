2eba5afc0c95519b28f77df441a927689b23e45e
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-02 20:15:14 -0700

    helper/terraform: Deploy (wip)
    Almost done, just artifact extraction isn't done.

---
+++ helper/terraform/deploy.go
@@ -0,0 +1,124 @@
+package terraform
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/hashicorp/otto/app"
+	"github.com/hashicorp/otto/directory"
+)
+
+type DeployOptions struct {
+	// Dir is the directory where Terraform is run. If this isn't set, it'll
+	// default to "#{ctx.Dir}/deploy".
+	Dir string
+}
+
+// Deploy deploys an application using Terraform.
+//
+// This will verify the infrastruction is created and a build is available,
+// and use that information to run Terraform. Any edge cases around Terraform
+// failures is handled and state storage is automatic as well.
+//
+// This function implements app.App.Deploy.
+func Deploy(ctx *app.Context, opts *DeployOptions) error {
+	// Get the infrastructure state. The infrastructure must be
+	// created for us to deploy to it.
+	infra, err := ctx.Directory.GetInfra(&directory.Infra{
+		Lookup: directory.Lookup{
+			Infra: ctx.Appfile.ActiveInfrastructure().Name}})
+	if err != nil {
+		return err
+	}
+
+	if infra == nil || infra.State != directory.InfraStateReady {
+		return fmt.Errorf(
+			"Infrastructure for this application hasn't been built yet.\n" +
+				"The deploy step requires this because the target infrastructure\n" +
+				"as well as its final properties can affect the deploy process.\n" +
+				"Please run `otto infra` to build the underlying infrastructure,\n" +
+				"then run `otto deploy` again.")
+	}
+
+	// Construct the variables for Terraform from our queried infra
+	vars := make(map[string]string)
+	for k, v := range infra.Outputs {
+		vars[k] = v
+	}
+	for k, v := range ctx.InfraCreds {
+		vars[k] = v
+	}
+
+	// Get the build information. We must have had a prior build in
+	// order to deploy.
+	build, err := ctx.Directory.GetBuild(&directory.Build{
+		App:         ctx.Tuple.App,
+		Infra:       ctx.Tuple.Infra,
+		InfraFlavor: ctx.Tuple.InfraFlavor,
+	})
+	if err != nil {
+		return err
+	}
+	if build == nil {
+		return fmt.Errorf(
+			"This application hasn't been built yet. Please run `otto build`\n" +
+				"first so that the deploy step has an artifact to deploy.")
+	}
+
+	// TODO: extract artifact info into variables...
+
+	// Get our old deploy to populate the old state data if we have it.
+	// This step is critical to make sure that Terraform remains idempotent
+	// and that it handles migrations properly.
+	//
+	// If we don't have a prior deploy, that is okay, we just create one
+	// now (with the DeployStateNew to note that we've never deployed). This
+	// gives us the UUID we can use for the state storage.
+	deployLookup := &directory.Deploy{
+		App:         ctx.Tuple.App,
+		Infra:       ctx.Tuple.Infra,
+		InfraFlavor: ctx.Tuple.InfraFlavor,
+	}
+	deploy, err := ctx.Directory.GetDeploy(&directory.Deploy{
+		App:         ctx.Tuple.App,
+		Infra:       ctx.Tuple.Infra,
+		InfraFlavor: ctx.Tuple.InfraFlavor,
+	})
+	if err != nil {
+		return err
+	}
+	if deploy == nil {
+		// If we have no deploy, put in a temporary one
+		deploy = deployLookup
+		deploy.State = directory.DeployStateNew
+
+		// Write the temporary deploy so we have an ID to use for the state
+		if err := ctx.Directory.PutDeploy(deploy); err != nil {
+			return err
+		}
+	}
+
+	// Get the directory
+	tfDir := opts.Dir
+	if tfDir == "" {
+		tfDir = filepath.Join(ctx.Dir, "deploy")
+	}
+
+	// Run Terraform!
+	tf := &Terraform{
+		Dir:       tfDir,
+		Ui:        ctx.Ui,
+		Variables: vars,
+		Directory: ctx.Directory,
+		StateId:   deploy.ID,
+	}
+	if err := tf.Execute("apply"); err != nil {
+		return fmt.Errorf(
+			"Error running Terraform: %s\n\n" +
+				"Terraform usually has helpful error messages. Please read the error\n" +
+				"messages above and resolve them. Sometimes simply running `otto deply`\n" +
+				"again will work.")
+	}
+
+	return nil
+}
