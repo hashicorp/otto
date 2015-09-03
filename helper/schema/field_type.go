fec45f7bb9fae0a6be84f6872a217cf4c0a3c4b8
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-03 13:43:51 -0700

    helper/schema: minimal field schema helpers

---
+++ helper/schema/field_type.go
@@ -0,0 +1,27 @@
+package schema
+
+// FieldType is the enum of types that a field can be.
+type FieldType uint
+
+const (
+	TypeInvalid FieldType = 0
+	TypeString  FieldType = iota
+	TypeInt
+	TypeBool
+	TypeMap
+)
+
+func (t FieldType) String() string {
+	switch t {
+	case TypeString:
+		return "string"
+	case TypeInt:
+		return "int"
+	case TypeBool:
+		return "bool"
+	case TypeMap:
+		return "map"
+	default:
+		return "unknown type"
+	}
+}
