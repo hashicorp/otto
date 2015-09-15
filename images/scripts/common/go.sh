1f24a419d323e3f396ba4b2424f3ed8b13e7b9f7
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-14 17:08:39 -0700

    images: install Go properly

--- images/scripts/common/go.sh
+++ images/scripts/common/go.sh
@@ -3,7 +3,7 @@
 GO_VERSION="1.5.1"
 
 # Download and install Go
-wget -q -O /tmp/go.tar.gz https://storage.googleapis.com/golang/${GO_VERSION}.linux-amd64.tar.gz
+wget -q -O /tmp/go.tar.gz https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
 tar -C /usr/local -xzf /tmp/go.tar.gz
 rm /tmp/go.tar.gz
 
