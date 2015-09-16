462ff4cb2dd66b1ef1579be6792477e0c5a1857b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-16 13:00:40 -0700

    appfile: basic validations

--- appfile/validate.go
+++ appfile/validate.go
@@ -1,6 +1,26 @@
 package appfile
 
+import (
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+)
+
 // Validate validates the Appfile
-func (*File) Validate() error {
-	return nil
+func (f *File) Validate() error {
+	var result error
+	if f.Application == nil {
+		result = multierror.Append(result, fmt.Errorf(
+			"'application' stanza required for Appfile"))
+	}
+	if f.Project == nil {
+		result = multierror.Append(result, fmt.Errorf(
+			"'project' stanza required for Appfile"))
+	}
+	if f.Infrastructure == nil {
+		result = multierror.Append(result, fmt.Errorf(
+			"'infra' stanza required for Appfile"))
+	}
+
+	return result
 }
---
+++ appfile/validate_test.go
@@ -0,0 +1,47 @@
+package appfile
+
+import (
+	"path/filepath"
+	"testing"
+)
+
+func TestFileValidate(t *testing.T) {
+	cases := []struct {
+		File string
+		Err  bool
+	}{
+		{
+			"validate-basic",
+			false,
+		},
+
+		{
+			"validate-no-app",
+			true,
+		},
+
+		{
+			"validate-no-project",
+			true,
+		},
+
+		{
+			"validate-no-infra",
+			true,
+		},
+	}
+
+	for _, tc := range cases {
+		f, err := ParseFile(filepath.Join("./test-fixtures", tc.File, "Appfile"))
+		if err != nil {
+			t.Fatalf("file:%s\n\n%s", tc.File, err)
+			continue
+		}
+
+		err = f.Validate()
+		if (err != nil) != tc.Err {
+			t.Fatalf("file: %s\n\n%s", tc.File, err)
+			continue
+		}
+	}
+}
