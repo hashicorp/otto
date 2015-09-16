5ccf555ae696391550971dea88e89d49aaec7277
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-15 21:35:40 -0700

    appfile: parallelize import compilation

--- appfile/compile.go
+++ appfile/compile.go
@@ -386,8 +386,6 @@ func compileImports(
 		return nil
 	}
 
-	// TODO: parallelization
-
 	// A graph is used to track for cycles
 	var graphLock sync.Mutex
 	graph := new(dag.AcyclicGraph)
@@ -398,13 +396,20 @@ func compileImports(
 	var resultErr error
 	var resultErrLock sync.Mutex
 
+	// Forward declarations for some nested functions we use. The docs
+	// for these functions are above each.
+	var importSingle func(parent string, f *File) bool
+	var downloadSingle func(string, *sync.WaitGroup, *sync.Mutex, []*File, int)
+
 	// importSingle is responsible for kicking off the imports and merging
 	// them for a single file. This will return true on success, false on
 	// failure. On failure, it is expected that any errors are appended to
 	// resultErr.
-	var importSingle func(parent string, f *File) bool
 	importSingle = func(parent string, f *File) bool {
+		var wg sync.WaitGroup
+
 		// Build the list of files we'll merge later
+		var mergeLock sync.Mutex
 		merge := make([]*File, len(f.Imports))
 
 		// Go through the imports and kick off the download
@@ -439,51 +444,20 @@ func compileImports(
 				}
 			}
 
-			// Call the callback if we have one
-			log.Printf("[DEBUG] loading import: %s", source)
-			if opts.Callback != nil {
-				opts.Callback(&CompileEventImport{
-					Source: source,
-				})
-			}
+			wg.Add(1)
+			go downloadSingle(source, &wg, &mergeLock, merge, idx)
+		}
 
-			// Download the dependency
-			if err := storage.Get(source, source, true); err != nil {
-				resultErrLock.Lock()
-				defer resultErrLock.Unlock()
-				resultErr = multierror.Append(resultErr, fmt.Errorf(
-					"Error loading import source: %s", err))
+		// Wait for completion
+		wg.Wait()
+
+		// Go through the merge list and look for any nil entries, which
+		// means that download failed. In that case, return immediately.
+		// We assume any errors were put into resultErr.
+		for _, importF := range merge {
+			if importF == nil {
 				return false
 			}
-			dir, _, err := storage.Dir(source)
-			if err != nil {
-				resultErrLock.Lock()
-				defer resultErrLock.Unlock()
-				resultErr = multierror.Append(resultErr, fmt.Errorf(
-					"Error loading import source: %s", err))
-				return false
-			}
-
-			// Parse the Appfile
-			importF, err := ParseFile(filepath.Join(dir, "Appfile"))
-			if err != nil {
-				resultErrLock.Lock()
-				defer resultErrLock.Unlock()
-				resultErr = multierror.Append(resultErr, fmt.Errorf(
-					"Error parsing Appfile in %s: %s", source, err))
-				return false
-			}
-
-			// We use the ID to store the source, but we clear it
-			// when we actually merge.
-			importF.ID = source
-
-			// Import the imports in this
-			if !importSingle(source, importF) {
-				return false
-			}
-
-			merge[idx] = importF
 		}
 
 		for _, importF := range merge {
@@ -502,6 +476,62 @@ func compileImports(
 		}
 
 		return true
+	}
+
+	// downloadSingle is used to download a single import and parse the
+	// Appfile. This is a separate function because it is generally run
+	// in a goroutine so we can parallelize grabbing the imports.
+	downloadSingle = func(source string, wg *sync.WaitGroup, l *sync.Mutex, result []*File, idx int) {
+		defer wg.Done()
+
+		// Call the callback if we have one
+		log.Printf("[DEBUG] loading import: %s", source)
+		if opts.Callback != nil {
+			opts.Callback(&CompileEventImport{
+				Source: source,
+			})
+		}
+
+		// Download the dependency
+		if err := storage.Get(source, source, true); err != nil {
+			resultErrLock.Lock()
+			defer resultErrLock.Unlock()
+			resultErr = multierror.Append(resultErr, fmt.Errorf(
+				"Error loading import source: %s", err))
+			return
+		}
+		dir, _, err := storage.Dir(source)
+		if err != nil {
+			resultErrLock.Lock()
+			defer resultErrLock.Unlock()
+			resultErr = multierror.Append(resultErr, fmt.Errorf(
+				"Error loading import source: %s", err))
+			return
+		}
+
+		// Parse the Appfile
+		importF, err := ParseFile(filepath.Join(dir, "Appfile"))
+		if err != nil {
+			resultErrLock.Lock()
+			defer resultErrLock.Unlock()
+			resultErr = multierror.Append(resultErr, fmt.Errorf(
+				"Error parsing Appfile in %s: %s", source, err))
+			return
+		}
+
+		// We use the ID to store the source, but we clear it
+		// when we actually merge.
+		importF.ID = source
+
+		// Import the imports in this
+		if !importSingle(source, importF) {
+			return
+		}
+
+		// Once we're done, acquire the lock and write it
+		l.Lock()
+		defer l.Unlock()
+		result[idx] = importF
 	}
 
 	importSingle("root", root)
