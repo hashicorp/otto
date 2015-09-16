f0ba92e75df4e2431f227a87bf316a1737b7e316
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-16 11:21:44 -0700

    appfile/detect: merging configs

--- appfile/detect/config.go
+++ appfile/detect/config.go
@@ -9,6 +9,15 @@ type Config struct {
 	Detectors []*Detector
 }
 
+// Merge merges another config into this one. This will modify this
+// Config object. Detectors in c2 are tried after detectors in this
+// Config. Conflicts are ignored as lower priority detectors, meaning that
+// if two detectors are for type "go", both will be tried.
+func (c *Config) Merge(c2 *Config) error {
+	c.Detectors = append(c.Detectors, c2.Detectors...)
+	return nil
+}
+
 // Detector is something that detects a single type.
 type Detector struct {
 	Type string
