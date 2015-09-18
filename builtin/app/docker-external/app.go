368c1847cc4b5d8fd6fcd3bb37e6ee5227173794
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-18 09:52:22 -0700

    app/docker-external

---
+++ builtin/app/docker-external/app.go
@@ -0,0 +1,96 @@
+package dockerext
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+
+	"github.com/hashicorp/otto/app"
+	"github.com/hashicorp/otto/foundation"
+	"github.com/hashicorp/otto/helper/bindata"
+	"github.com/hashicorp/otto/helper/compile"
+	"github.com/hashicorp/otto/helper/schema"
+	"github.com/hashicorp/otto/helper/vagrant"
+)
+
+//go:generate go-bindata -pkg=dockerext -nomemcopy -nometadata ./data/...
+
+// App is an implementation of app.App
+type App struct{}
+
+func (a *App) Compile(ctx *app.Context) (*app.CompileResult, error) {
+	fragmentPath := filepath.Join(ctx.Dir, "dev-dep", "Vagrantfile.fragment")
+
+	var opts compile.AppOptions
+	custom := &customizations{Opts: &opts}
+	opts = compile.AppOptions{
+		Ctx: ctx,
+		FoundationConfig: foundation.Config{
+			ServiceName: ctx.Application.Name,
+		},
+		Bindata: &bindata.Data{
+			Asset:    Asset,
+			AssetDir: AssetDir,
+			Context: map[string]interface{}{
+				"fragment_path": fragmentPath,
+				"path": map[string]string{
+					"guest_working": fmt.Sprintf(
+						"/otto-deps/%s-%s",
+						ctx.Application.Name,
+						ctx.Appfile.ID),
+				},
+			},
+		},
+		Customizations: []*compile.Customization{
+			&compile.Customization{
+				Type:     "docker",
+				Callback: custom.processDocker,
+				Schema: map[string]*schema.FieldSchema{
+					"image": &schema.FieldSchema{
+						Type:        schema.TypeString,
+						Default:     "",
+						Description: "Image name to run",
+					},
+
+					"run_args": &schema.FieldSchema{
+						Type:        schema.TypeString,
+						Default:     "",
+						Description: "Args to pass to `docker run`",
+					},
+				},
+			},
+		},
+	}
+
+	return compile.App(&opts)
+}
+
+func (a *App) Build(ctx *app.Context) error {
+	return nil
+}
+
+func (a *App) Deploy(ctx *app.Context) error {
+	return nil
+}
+
+func (a *App) Dev(ctx *app.Context) error {
+	return vagrant.Dev(&vagrant.DevOptions{
+		Instructions: strings.TrimSpace(devInstructions),
+	}).Route(ctx)
+}
+
+func (a *App) DevDep(dst, src *app.Context) (*app.DevDep, error) {
+	// Nothing needs to be done for this
+	return nil, nil
+}
+
+const devInstructions = `
+A development has been created.
+
+Note that this development environment is just an example of what a
+consumer of this application might see as a development dependency.
+"docker-external" application types represent Docker images that are
+developed and built external to Otto. A future version of Otto will include
+a native "docker" type for a Docker-based development workflow. For
+the "docker-external" type, the specified docker image is started.
+`
---
+++ builtin/app/docker-external/data/common/dev/Vagrantfile.tpl
@@ -0,0 +1,44 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "hashicorp/precise64"
+
+  # Setup some stuff
+  config.vm.provision "shell", inline: $script
+
+  # Foundation configuration (if any)
+  {% for dir in foundation_dirs.dev %}
+  dir = "/otto/foundation-{{ forloop.Counter }}"
+  config.vm.synced_folder "{{ dir }}", dir
+  config.vm.provision "shell", inline: "cd #{dir} && bash #{dir}/main.sh"
+  {% endfor %}
+
+  # Read in the fragment that we use as a dep
+  eval(File.read("{{ fragment_path }}"), binding)
+
+  ["vmware_fusion", "vmware_workstation"].each do |name|
+    config.vm.provider(name) do |p|
+      p.enable_vmrun_ip_lookup = false
+    end
+  end
+end
+
+$script = <<SCRIPT
+set -e
+
+# otto-exec: execute command with output logged but not displayed
+oe() { $@ 2>&1 | logger -t otto > /dev/null; }
+
+# otto-log: output a prefixed message
+ol() { echo "[otto] $@"; }
+
+# Configuring SSH for faster login
+if ! grep "UseDNS no" /etc/ssh/sshd_config >/dev/null; then
+  echo "UseDNS no" | sudo tee -a /etc/ssh/sshd_config >/dev/null
+  oe sudo service ssh restart
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+ol "Installing HTTPS driver for Apt..."
+oe sudo apt-get update
+oe sudo apt-get install -y apt-transport-https
+SCRIPT
