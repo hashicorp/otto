2e4374234a447d8021dd160eeb91c0b32a1b691f
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-09-27 23:13:49 -0700

    website: update deploy stuff

--- website/config.rb
+++ website/config.rb
@@ -13,7 +13,7 @@ set :base_url, "https://www.ottoproject.io/"
 
 activate :hashicorp do |h|
   h.version         = ENV["OTTO_VERSION"]
-  h.bintray_enabled = ENV["BINTRAY_ENABLED"]
+  h.bintray_enabled = ENV["BINTRAY_ENABLED"] == "1"
   h.bintray_repo    = "mitchellh/otto"
   h.bintray_user    = "mitchellh"
   h.bintray_key     = ENV["BINTRAY_API_KEY"]
