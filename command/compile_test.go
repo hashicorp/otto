5d32edc4fa866030795e2ea5aabccb5246046fc5
Paul Hinze <phinze@phinze.com>
2015-10-01 11:17:28 -0400

    command/compile: Fix implicit app name from dir

--- command/compile_test.go
+++ command/compile_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 
 	"github.com/hashicorp/otto/app"
+	"github.com/hashicorp/otto/appfile/detect"
+	"github.com/hashicorp/otto/foundation"
 	"github.com/hashicorp/otto/otto"
 	"github.com/mitchellh/cli"
 )
@@ -36,6 +38,56 @@ func TestCompile(t *testing.T) {
 	}
 	if !appImpl.CompileCalled {
 		t.Fatal("Compile should be called")
+	}
+}
+
+func TestCompile_noAppFile(t *testing.T) {
+	core := otto.TestCoreConfig(t)
+	infra := otto.TestInfra(t, "aws", core)
+	appImpl := otto.TestApp(t, app.Tuple{"test", "aws", "simple"}, core)
+	appImpl.CompileFunc = func(ctx *app.Context) (*app.CompileResult, error) {
+		if ctx.Application == nil {
+			t.Fatal("application unexpectedly nil")
+		}
+		if ctx.Application.Name != "compile-no-appfile" {
+			t.Fatalf("expected: compile-no-appfile; got: %s", ctx.Application.Name)
+		}
+		return nil, nil
+	}
+	foundImpl := otto.TestFoundation(
+		t, foundation.Tuple{"consul", "aws", "simple"}, core)
+	ui := new(cli.MockUi)
+	detectors := []*detect.Detector{
+		&detect.Detector{
+			Type: "test",
+			File: []string{"test-file"},
+		},
+	}
+	c := &CompileCommand{
+		Meta: Meta{
+			CoreConfig: core,
+			Ui:         ui,
+		},
+		Detectors: detectors,
+	}
+
+	dir := fixtureDir("compile-no-appfile")
+	defer os.Remove(filepath.Join(dir, ".ottoid"))
+	defer testChdir(t, dir)()
+
+	args := []string{}
+	if code := c.Run(args); code != 0 {
+		t.Fatalf("bad: %d\n\n%s", code, ui.ErrorWriter.String())
+	}
+
+	if !infra.CompileCalled {
+		t.Fatal("Compile should be called")
+	}
+	if !appImpl.CompileCalled {
+		t.Fatal("Compile should be called")
+	}
+	if !foundImpl.CompileCalled {
+		t.Fatal("Foundation should be called")
 	}
 }
 
