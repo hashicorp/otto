94f467ae08c0a044549abdbfa2c4f3b39d1b648e
Jeyvison Nascimento <jeynoronha@gmail.com>
2015-10-20 01:21:11 +0000

    Creating appFile

--- command/init.go
+++ command/init.go
@@ -1,11 +1,27 @@
 package command
 
+import (
+	"fmt"
+	"io/ioutil"
+)
+
 type InitCommand struct {
 	Meta
 }
 
+type FileConfigurations struct {
+	Language string
+}
+
+var appFile = `application {
+	name = "otto"
+	type = "%v"
+}\n`
+
 func (c *InitCommand) Run(args []string) int {
-	return 1
+	file := FileConfigurations{"go"}
+	generateAppFile(file)
+	return 0
 }
 
 func (c *InitCommand) Synopsis() string {
@@ -14,4 +30,9 @@ func (c *InitCommand) Synopsis() string {
 
 func (c *InitCommand) Help() string {
 	return "I need somebody..."
+}
+
+func generateAppFile(config FileConfigurations) {
+	appFileString := fmt.Sprintf(appFile, "jey")
+	ioutil.WriteFile("Appfile", []byte(appFileString), 0644)
 }
