36a1a8e0f2a639bece747790262d941b3ffb8242
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2015-12-03 10:08:47 -0800

    forgot a file

---
+++ helper/compile/custom_vagrant.go
@@ -0,0 +1,29 @@
+package compile
+
+import (
+	"github.com/hashicorp/otto/helper/schema"
+)
+
+// VagrantCustomizations returns common Vagrant customizations that work
+// with the default settings of the app compilation helper.
+func VagrantCustomizations(opts *AppOptions) []*Customization {
+	return []*Customization{
+		&Customization{
+			Type:     "vagrant",
+			Callback: vagrantCustomizationCallback(opts),
+			Schema: map[string]*schema.FieldSchema{
+				"vagrantfile": &schema.FieldSchema{
+					Type:        schema.TypeString,
+					Description: "Vagrantfile contents to append for development.",
+				},
+			},
+		},
+	}
+}
+
+func vagrantCustomizationCallback(opts *AppOptions) CustomizationFunc {
+	return func(d *schema.FieldData) error {
+		opts.Bindata.Context["dev_extra_vagrantfile"] = d.Get("vagrantfile")
+		return nil
+	}
+}
