0ce04ee56a4ddf3a72a5e9b79648311e47b85049
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-01-06 10:41:09 -0800

    website: 0.2.0

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.ottoproject.io/"
 
 activate :hashicorp do |h|
   h.name        = "otto"
-  h.version     = "0.1.2"
+  h.version     = "0.2.0"
   h.github_slug = "hashicorp/otto"
 
   h.minify_javascript = false
