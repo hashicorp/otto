f1be4b08568e14f0cb67f949c6b48dbacfc5c209
Seth Vargo <sethvargo@gmail.com>
2016-01-12 17:39:41 -0500

    Deploy to S3 fronted by Fastly using s3cmd and magic

--- scripts/website_push.sh
+++ scripts/website_push.sh
@@ -1,40 +1,73 @@
 #!/bin/bash
+set -e
 
-# Set the tmpdir
-if [ -z "$TMPDIR" ]; then
-  TMPDIR="/tmp"
+PROJECT="otto"
+FASTLY_SERVICE_ID="7GrxRJP3PVBuqQbyxYQ0MV"
+
+# Ensure the proper AWS environment variables are set
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "Missing AWS_ACCESS_KEY_ID!"
+  exit 1
 fi
 
-# Create a temporary build dir and make sure we clean it up. For
-# debugging, comment out the trap line.
-DEPLOY=`mktemp -d $TMPDIR/otto-www-XXXXXX`
-trap "rm -rf $DEPLOY" INT TERM EXIT
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "Missing AWS_SECRET_ACCESS_KEY!"
+  exit 1
+fi
 
-# Get the parent directory of where this script is.
+# Ensure the proper Fastly keys are set
+if [ -z "$FASTLY_API_KEY" ]; then
+  echo "Missing FASTLY_API_KEY!"
+  exit 1
+fi
+
+# Ensure we have s3cmd installed
+if ! command -v "s3cmd" >/dev/null 2>&1; then
+  echo "Missing s3cmd!"
+  exit 1
+fi
+
+# Get the parent directory of where this script is and change into our website
+# directory
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
-# Copy into tmpdir
-shopt -s dotglob
-cp -r $DIR/website/* $DEPLOY/
+# Upload the files to S3 - we disable mime-type detection by the python library
+# and just guess from the file extension because it's surprisingly more
+# accurate, especially for CSS and javascript. We also tag the uploaded files
+# with the proper Surrogate-Key, which we will later purge in our API call to
+# Fastly.
+if [ -z "$NO_UPLOAD" ]; then
+  echo "Uploading to S3..."
 
-# Change into that directory
-pushd $DEPLOY &>/dev/null
+  # Check that the site has been built
+  if [ ! -d "$DIR/website/build" ]; then
+    echo "Missing compiled website! Running `make build` to compile!"
+    exit 1
+  fi
 
-# Ignore some stuff
-touch .gitignore
-echo ".sass-cache" >> .gitignore
-echo "build" >> .gitignore
-echo "vendor" >> .gitignore
+  s3cmd \
+    --quiet \
+    --guess-mime-type \
+    --no-mime-magic \
+    --acl-public \
+    --recursive \
+    --add-header="Cache-Control: max-age=31536000" \
+    --add-header="x-amz-meta-surrogate-key: site-$PROJECT" \
+    put "$DIR/website/build/" "s3://hc-sites/$PROJECT/latest/"
+fi
 
-# Add everything
-git init -q .
-git add .
-git commit -q -m "Deploy by $USER"
-
-git remote add heroku git@heroku.com:otto-www.git
-git push -f heroku master
-
-# Go back to our root
-popd &>/dev/null
+# Perform a soft-purge of the surrogate key.
+if [ -z "$NO_PURGE" ]; then
+  echo "Purging Fastly cache..."
+  curl \
+    --fail \
+    --silent \
+    --output /dev/null \
+    --request "POST" \
+    --header "Accept: application/json" \
+    --header "Fastly-Key: $FASTLY_API_KEY" \
+    --header "Fastly-Soft-Purge: 1" \
+    "https://api.fastly.com/service/$FASTLY_SERVICE_ID/purge/site-$PROJECT"
+fi
