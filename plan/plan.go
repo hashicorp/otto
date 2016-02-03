bfa5694c81569d36be4210e773c3e4deb4049c10
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-02-03 09:14:31 +0100

    plan: Refs in tasks

--- plan/plan.go
+++ plan/plan.go
@@ -6,6 +6,8 @@ package plan
 import (
 	"fmt"
 
+	"github.com/hashicorp/hil"
+	"github.com/hashicorp/hil/ast"
 	"github.com/hashicorp/otto/context"
 )
 
@@ -50,6 +52,45 @@ type TaskExecutor interface {
 	// Execute is called to run a task. It is given access to a populated
 	// shared context and the map of arguments.
 	Execute(ctx *context.Shared, args map[string]*TaskArg) (map[string]*TaskResult, error)
+}
+
+//-------------------------------------------------------------------
+// Methods on structs
+//-------------------------------------------------------------------
+
+// Refs returns the references to other variables found within the
+// argument.
+func (t *TaskArg) Refs() []string {
+	s, ok := t.Value.(string)
+	if !ok {
+		// If it isn't a string it can't reference any other vars
+		return nil
+	}
+
+	// Parse the value as HIL
+	root, err := hil.Parse(s)
+	if err != nil {
+		// Panic, this should've been validated earlier
+		panic(err)
+	}
+
+	// Walk the AST and find all variable access
+	varMap := make(map[string]struct{})
+	root.Accept(func(n ast.Node) ast.Node {
+		if vn, ok := n.(*ast.VariableAccess); ok {
+			varMap[vn.Name] = struct{}{}
+		}
+
+		return n
+	})
+
+	// Create the result slice from the map so that all values are unique
+	result := make([]string, 0, len(varMap))
+	for k, _ := range varMap {
+		result = append(result, k)
+	}
+
+	return result
 }
 
 //-------------------------------------------------------------------
