71bc15d05a2b0e3236fb478cf95a069c2753de47
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-02-17 19:45:15 +0000

    task/terraform: test partial state save

--- builtin/task/terraform/apply_test.go
+++ builtin/task/terraform/apply_test.go
@@ -134,3 +134,55 @@ func TestApply_infraCreds(t *testing.T) {
 		}
 	}
 }
+
+// Tests that Apply will save a partial state in the case of an error
+func TestApply_partial(t *testing.T) {
+	if !hasTF {
+		t.Skip("Terraform not found")
+	}
+
+	var task ApplyTask
+
+	// Build the args
+	infraName := "foo"
+	ctx := context.TestShared(t)
+	pwd := filepath.Join("./test-fixtures", "apply-partial")
+	args := &plan.ExecArgs{
+		Output: ioutil.Discard,
+		Extra:  map[string]interface{}{"context": ctx},
+		Args: map[string]*plan.TaskArg{
+			"pwd":   &plan.TaskArg{Value: pwd},
+			"infra": &plan.TaskArg{Value: infraName},
+		},
+	}
+
+	// Initialize the infra
+	lookup := &directory.InfraLookup{Name: infraName}
+	infra := &directory.Infra{Name: infraName}
+	if err := ctx.Directory.PutInfra(lookup, infra); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Validate
+	if _, err := task.Validate(args); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Execute
+	_, err := task.Execute(args)
+	if err == nil {
+		t.Fatal("should error")
+	}
+
+	// Verify the state was updated
+	infra, err = ctx.Directory.GetInfra(lookup)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if len(infra.Opaque) <= 0 {
+		t.Fatal("should have state")
+	}
+	if infra.State != directory.InfraStatePartial {
+		t.Fatal("state should be ready")
+	}
+}
