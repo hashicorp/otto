f0b5f18a16233c1b728fb44d0493d919642f0e8b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2016-02-17 19:40:14 +0000

    task/terraform: test that infra creds are passed through

--- builtin/task/terraform/apply_test.go
+++ builtin/task/terraform/apply_test.go
@@ -79,3 +79,58 @@ func TestApply(t *testing.T) {
 		}
 	}
 }
+
+// Test that Apply passes in the infra creds to the apply
+func TestApply_infraCreds(t *testing.T) {
+	if !hasTF {
+		t.Skip("Terraform not found")
+	}
+
+	var task ApplyTask
+
+	// Build the args
+	infraName := "foo"
+	ctx := context.TestShared(t)
+	pwd := filepath.Join("./test-fixtures", "apply-infracreds")
+	args := &plan.ExecArgs{
+		Output: ioutil.Discard,
+		Extra:  map[string]interface{}{"context": ctx},
+		Args: map[string]*plan.TaskArg{
+			"pwd":   &plan.TaskArg{Value: pwd},
+			"infra": &plan.TaskArg{Value: infraName},
+		},
+	}
+
+	// Set some infra creds
+	ctx.InfraCreds = map[string]string{"foo": "bar"}
+
+	// Initialize the infra
+	lookup := &directory.InfraLookup{Name: infraName}
+	infra := &directory.Infra{Name: infraName}
+	if err := ctx.Directory.PutInfra(lookup, infra); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Validate
+	if _, err := task.Validate(args); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Execute
+	if _, err := task.Execute(args); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Verify the state was updated
+	infra, err := ctx.Directory.GetInfra(lookup)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	{
+		expected := map[string]string{"bar": "bar!"}
+		if !reflect.DeepEqual(infra.Outputs, expected) {
+			t.Fatalf("bad: %#v", infra.Outputs)
+		}
+	}
+}
