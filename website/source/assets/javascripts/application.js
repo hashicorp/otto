70e22205602cfe7709a5caedb0d4d3214a2e8d36
captainill <jonthomas0@gmail.com>
2016-08-19 12:16:24 -0700

    initial incubate site

--- website/source/assets/javascripts/application.js
+++ website/source/assets/javascripts/application.js
@@ -1,10 +1,1 @@
-//= require jquery
-//= require bootstrap
 
-//= require lib/_jquery.waypoints
-//= require lib/_String.substitute
-//= require lib/_Function.prototype.bind
-//= require lib/_Base
-
-//= require app/_Sidebar
-//= require app/_Init
